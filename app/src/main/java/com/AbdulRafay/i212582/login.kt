a06e7b182698522e955d4119666c430ff7c33767
rafay-47 <abdulrafay1747@gmail.com>
2024-02-06 01:53:21 +0500

    Page 2

---
+++ app/src/main/java/com/AbdulRafay/i212582/login.kt
@@ -0,0 +1,29 @@
+package com.AbdulRafay.i212582
+
+import android.content.Intent
+import android.graphics.Color
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.text.method.LinkMovementMethod
+import android.widget.Button
+import android.widget.TextView
+
+
+class login : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.login_page)
+
+        txt_nav()
+
+
+
+    }
+
+    fun txt_nav(){
+        var txt:TextView = findViewById(R.id.textView9)
+        txt.setOnClickListener {
+            startActivity(Intent(this,signup::class.java))
+        }
+    }
+}
---
+++ app/src/main/java/com/AbdulRafay/i212582/signup.kt
@@ -0,0 +1,17 @@
+package com.AbdulRafay.i212582
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+
+
+class signup : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.signup_page)
+
+
+
+    }
+}
