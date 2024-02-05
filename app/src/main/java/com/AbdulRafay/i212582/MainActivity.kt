44478c2d5880e1b7e93b0474f6633bb9311572c5
rafay-47 <abdulrafay1747@gmail.com>
2024-02-05 22:07:33 +0500

    first commit

---
+++ app/src/main/java/com/AbdulRafay/i212582/MainActivity.kt
@@ -0,0 +1,19 @@
+package com.AbdulRafay.i212582
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.ImageView
+import android.graphics.PorterDuff
+
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.login_page)
+
+
+
+
+
+    }
+}
