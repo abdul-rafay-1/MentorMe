4a893cc3c24a598f0318cb60e0e3d5264332ebd6
rafay-47 <abdulrafay1747@gmail.com>
2024-02-06 01:51:50 +0500

    Page 2

--- app/src/main/java/com/AbdulRafay/i212582/MainActivity.kt
+++ app/src/main/java/com/AbdulRafay/i212582/MainActivity.kt
@@ -1,19 +1,21 @@
 package com.AbdulRafay.i212582
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.ImageView
-import android.graphics.PorterDuff
+import android.view.View
+import android.widget.Button
 
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.login_page)
+        setContentView(R.layout.activity_main)
 
-
-
-
+        var btn:Button = findViewById(R.id.button)
+        btn.setOnClickListener {
+                startActivity(Intent(this,login::class.java))
+        }
 
     }
 }
