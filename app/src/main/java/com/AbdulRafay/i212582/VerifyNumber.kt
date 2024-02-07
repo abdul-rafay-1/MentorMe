1de623dae7fc5391cdfe9f6db12af955b9838ac4
rafay-47 <abdulrafay1747@gmail.com>
2024-02-07 12:52:56 +0500

    page 4

--- app/src/main/java/com/AbdulRafay/i212582/VerifyNumber.kt
+++ app/src/main/java/com/AbdulRafay/i212582/VerifyNumber.kt
@@ -1,11 +1,8 @@
 package com.AbdulRafay.i212582
 
 import android.content.Intent
-import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.View
-import android.widget.Button
 import android.widget.ImageButton
 
 
