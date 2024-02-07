d952c8c5bbd6d800d2c93bd12383b11c03cacbad
rafay-47 <abdulrafay1747@gmail.com>
2024-02-07 16:58:00 +0500

    page 4

--- app/src/main/java/com/AbdulRafay/i212582/ForgotPassword.kt
+++ app/src/main/java/com/AbdulRafay/i212582/ForgotPassword.kt
@@ -1,24 +1,23 @@
 package com.AbdulRafay.i212582
 
+
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.Button
+import android.widget.ImageButton
 import android.widget.TextView
 
 
-class signup : AppCompatActivity() {
+class ForgotPassword : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.signup_page)
+        setContentView(R.layout.forgot_password)
 
-        val btn:Button = findViewById(R.id.button3)
-        btn.setOnClickListener {
-            startActivity(Intent(this,VerifyNumber::class.java))
-        }
-        val txt:TextView = findViewById(R.id.textView22)
+        val txt:TextView = findViewById(R.id.textView32)
+
         txt.setOnClickListener{
             startActivity(Intent(this,login::class.java))
         }
+
     }
 }
--- app/src/main/java/com/AbdulRafay/i212582/login.kt
+++ app/src/main/java/com/AbdulRafay/i212582/login.kt
@@ -19,5 +19,10 @@ class login : AppCompatActivity() {
             startActivity(Intent(this,signup::class.java))
         }
 
+        val txt2:TextView = findViewById(R.id.textView7)
+        txt2.setOnClickListener {
+            startActivity(Intent(this,ForgotPassword::class.java))
+        }
+
     }
 }
