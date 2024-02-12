3a6a855582f789881c03d51013647a3fd21404ab
rafay-47 <abdulrafay1747@gmail.com>
2024-02-13 02:24:54 +0500

    page 21

---
+++ app/src/main/java/com/AbdulRafay/i212582/EditProfile.kt
@@ -0,0 +1,19 @@
+package com.AbdulRafay.i212582
+
+import android.content.Intent
+import android.graphics.Color
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.text.method.LinkMovementMethod
+import android.widget.Button
+import android.widget.TextView
+
+
+class EditProfile : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.edit_profile)
+
+
+    }
+}
